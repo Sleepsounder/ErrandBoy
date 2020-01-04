e91c50a834f7d44ce1b700cbf1f4254449b01e55
Geoff <geoff.register@gmail.com>
2020-01-04 13:42:39 -0500

    add test for missing title

---
+++ spec/requests/tasks/create_spec.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'POST /tasks', type: :request do
+  subject do
+    post '/tasks', params: params
+  end
+
+  let(:params) do
+    { task: {
+      title: title,
+      description: description
+    } }
+  end
+  let(:title) { 'Do some stuff.' }
+  let(:description) { 'A description of the stuff to be done.' }
+
+  context 'with no title' do
+    let(:title) { nil }
+
+    it 'returns a 422' do
+      expect(subject).to eq(422)
+    end
+  end
+
+  #   context "with valid params" do
+  #     let(:created_rating) { Rating.find_by(order_id: order.id) }
+  #     let(:params) do
+  #       {
+  #         "rater_id": rater.id,
+  #         "rated_id": rated.id,
+  #         "order_id": order.id,
+  #         "score": 5,
+  #         "review": "Great service!!"
+  #       }
+  #     end
+
+  #     it "creates a rating with the correct params" do
+  #       expect { subject }.to change(Rating, :count).by(1)
+  #       expect(created_rating.rater_id).to eq(params[:rater_id])
+  #       expect(created_rating.rated_id).to eq(params[:rated_id])
+  #       expect(created_rating.order_id).to eq(params[:order_id])
+  #       expect(created_rating.score).to eq(params[:score])
+  #       expect(created_rating.review).to eq(params[:review])
+  #     end
+  #   end
+end
