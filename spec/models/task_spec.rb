0efdcff310a4bb3cdcf13a63919685191a55ca79
Geoff <geoff.register@gmail.com>
2020-01-05 20:11:01 -0500

    Task Model spec for basic title validation

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { described_class.new(title: "Task #1") }
+
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+
+  it "is invalid without a title" do
+    subject.title = nil
+    expect(subject).to_not be_valid
+  end
 end
